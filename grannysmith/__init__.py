ec64fa63f7c1d48b693e5912f66cb73635fe5d71
Donny Kurniawan <donny.kurniawan@gmail.com>
2016-10-13 22:44:42 +1100

    First implementation

--- grannysmith/__init__.py
+++
@@ -1,2 +0,0 @@
-def joke():
-    return u'Hello World'
--- grannysmith/command_line.py
+++ grannysmith/command_line.py
@@ -1,7 +1,8 @@
-import grannysmith
 import click
+import requests
 
 from .items import *
+
 
 @click.command()
 @click.option('--postcode', default='3000')
@@ -12,30 +13,36 @@ def main(postcode, model, finish, storage):
     """grannysmith checks stock levels for the specified Apple iPhones
     around a particular Australian postcode."""
 
-    # click.echo(grannysmith.joke())
-    #click.echo('Postcode %s' % postcode)
-    # click.echo(items)
-
     click.echo(click.style('Searching for ', fg='yellow'), nl=False)
     click.echo(click.style('iPhone %s' % model, fg='red'), nl=False)
     click.echo(click.style(' %s' % finish, fg='green'), nl=False)
     click.echo(click.style(' %sGB' % storage, fg='blue'), nl=False)
-    click.echo(click.style(' in Apple stores around postcode', fg='yellow'), nl=False)
+    click.echo(click.style(
+        ' in Apple stores around postcode', fg='yellow'), nl=False)
     click.echo(click.style(' %s' % postcode, fg='cyan'), nl=True)
 
-    #tuples = [(index, item['id']) for index, item in enumerate(items)]
-    #string = "&".join("parts.%s=%s" % tuple for tuple in tuples)
-    # print(string)
+    products = [item['id']
+                for item in items
+                if item['model'] == model
+                and item['finish'] == finish
+                and item['storage'] == storage
+                ]
+    products_req = "&".join("parts.%s=%s" % (index, item)
+                            for index, item in enumerate(products))
 
-    tuples = [(index, item['id'])
-              for index, item in enumerate(items)
-              if item['model'] == model
-              and item['finish'] == finish
-              and item['storage'] == storage
-              ]
-    string = "&".join("parts.%s=%s" % tuple for tuple in tuples)
-    print(tuples)
+    request_url = 'http://www.apple.com/au/shop/retail/pickup-message?location=%s&%s' % (
+        postcode, products_req)
 
+    data = requests.get(request_url)
+    data = data.json()
 
-
-
+    for store in data['body']['stores']:
+        for product in products:
+            status = store['partsAvailability'][product]['pickupDisplay']
+            if status == 'available':
+                click.echo(click.style('Available', fg='green'), nl=False)
+                click.echo(click.style(' in ', fg='yellow'), nl=False)
+                click.echo(click.style(store['storeName'], fg='green'), nl=True)
+            else:
+                click.echo(click.style('Unavailable', fg='red'), nl=False)
+                click.echo(' in %s' % store['storeName'])
